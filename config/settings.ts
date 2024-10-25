60d215574c1c126138c140f74a88ed9ece1ba869
tiagosopris <12user12b12@gmail.com>
2024-10-25 19:34:08 -0300

    v1.1

--- config/settings.ts
+++ config/settings.ts
@@ -24,6 +24,7 @@ export const settings = {
         cyanMagenta: { colors: ['#00FFFF', '#FF00FF', '#40E0D0', '#FF1493'], isPremium: false },
         purpleBlue: { colors: ['#6A5ACD', '#8A2BE2', '#483D8B', '#7B68EE'], isPremium: false },
         tealOrange: { colors: ['#008080', '#FFA07A', '#20B2AA', '#FF6347'], isPremium: false },
+        smythOs: { colors: ['#45c9a9', '#13947d', '#45c9a9', '#45c9a9'], isPremium: false },
         gold: { colors: ['#FFD700', '#FFEA00', '#FFC700', '#FFA500'], isPremium: true },
         coralPeach: { colors: ['#FF7F50', '#FFDAB9', '#FF6347', '#FFE4E1'], isPremium: true },
         mintChocolate: { colors: ['#F5FFFA', '#D2691E', '#98FF98', '#8B4513'], isPremium: true },
@@ -31,7 +32,6 @@ export const settings = {
         magentaLime: { colors: ['#FF00FF', '#32CD32', '#FF1493', '#00FA9A'], isPremium: true },
         deepSea: { colors: ['#20B2AA', '#4682B4', '#5F9EA0', '#2E8B57'], isPremium: true },
         sunset: { colors: ['#FF4500', '#FF6347', '#FF8C00', '#FFD700'], isPremium: true },
-        pastelRainbow: { colors: ['#FFB6C1', '#FFD700', '#98FB98', '#ADD8E6', '#FF69B4'], isPremium: true },
-        smythOs: { colors: ['#45c9a9', '#13947d', '#45c9a9', '#45c9a9'], isPremium: false }
+        pastelRainbow: { colors: ['#FFB6C1', '#FFD700', '#98FB98', '#ADD8E6', '#FF69B4'], isPremium: true }
     }
 }
